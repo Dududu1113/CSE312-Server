93065a8504a8c0a01e4fe7725d936d3d70b9a0a3
Zhiwei Zhong <zzhong5@buffalo.edu>
2025-02-14 10:25:37 -0500

    done for response

--- util/response.py
+++ util/response.py
@@ -1,30 +1,53 @@
 import json
+from http import cookies
 
 
 class Response:
     def __init__(self):
+        self.statusCode = 200
+        self.statusText = 'OK'
+        self.addHeaders = {}
+        self.addCookies = {}
+        self.addBody = b''
         pass
 
     def set_status(self, code, text):
-        pass
+        self.statusCode = code
+        self.statusText = text
+        return self
 
     def headers(self, headers):
-        pass
+        self.addHeaders.update(headers)
+        return self
 
     def cookies(self, cookies):
-        pass
+        self.addCookies.update(cookies)
+        return self
 
     def bytes(self, data):
-        pass
+        self.addBody += data
+        return self
 
     def text(self, data):
-        pass
+        self.addBody += data.encode()
+        return self
 
     def json(self, data):
-        pass
+        self.addBody = json.dumps(data).encode()
+        self.addHeaders['Content-Type'] = 'application/json'
+        return self
 
     def to_data(self):
-        return b''
+        if "Content-Type" not in self.addHeaders:
+            self.addHeaders["Content-Type"] = "text/plain; charset=utf-8"
+        url = "HTTP/1.1 " + str(self.statusCode) + " " + self.statusText + "\r\n"
+        for key in self.addHeaders:
+            url += key + ": " + self.addHeaders[key] + "\r\n"
+        for key in self.addCookies:
+            url += key + "=" + self.addCookies[key] + "\r\n"
+        url += "\r\n"
+        url = url.encode()+self.addBody
+        return url
 
 
 def test1():
