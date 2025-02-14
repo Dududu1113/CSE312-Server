13c6399d340fde97ce66e8539ad77167d0702e2b
ZhiweiZhong <zzhong5@buffalo.edu>
2025-02-14 15:10:58 -0500

    done for router

--- util/router.py
+++ util/router.py
@@ -1,10 +1,14 @@
 class Router:
 
     def __init__(self):
-        pass
+        self.routes=[]
 
     def add_route(self, method, path, action, exact_path=False):
-        pass
+        self.routes.append([method,path,action,exact_path])
 
     def route_request(self, request, handler):
-        pass
+        for method, path, action, exact_path in self.routes:
+            if request.method == method:
+                if (exact_path and request.path == path) or (not exact_path and request.path.startswith(path)):
+                    action(request, handler)
+                    return
