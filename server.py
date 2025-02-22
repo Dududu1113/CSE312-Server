50f365315862b56d9da2ef64bea07aa5d05e08d7
Zhiwei Zhong <zzhong5@buffalo.edu>
2025-02-22 17:57:07 -0500

    AOS

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import os
 import socketserver
 import uuid
 import html
+import requests
 from util.request import Request
 from util.response import Response
 from util.router import Router
@@ -14,7 +15,7 @@ db = client["server"]
 messages_collection = db["CSE312"]
 
 def publicfile(request, handler):
-    mineType = {".html": "text/html",".css": "text/css",".js": "text/javascript",".jpg": "image/jpeg",".ico": "image/x-icon",".gif": "image/gif",".webp": "image/webp",".png": "image/png",".json": "application/json"}
+    mineType = {".html": "text/html",".css": "text/css",".js": "text/javascript",".jpg": "image/jpeg",".ico": "image/x-icon",".gif": "image/gif",".webp": "image/webp",".png": "image/png",".json": "application/json",".svg":"image/svg+xml"}
     path = request.path.replace("/","",1)
 
     if os.path.exists(path):
@@ -52,33 +53,59 @@ def render(request, handler, page_file):
         res = Response().set_status(404, "Not Found")
         handler.request.sendall(res.to_data())
 
+
 def create_chat(request, handler):
     res = Response()
     body = json.loads(request.body.decode())
     content = html.escape(body.get("content", ""))
 
     session_id = request.cookies.get("session")
-    # print(request.cookies)
-    # print("session_id", session_id)
     chat_id = str(uuid.uuid4())
     if not session_id:
+
         session_id = str(uuid.uuid4())
         author = f"User-{session_id[:8]}"
+
+        response = requests.get(f"https://api.dicebear.com/9.x/croodles-neutral/svg?seed={session_id}")
+        if response.status_code == 200:
+            profile_pic_dir = "public/imgs/profile-pics"
+            os.makedirs(profile_pic_dir, exist_ok=True)
+            profile_pic_path = f"{profile_pic_dir}/{session_id}.svg"
+            with open(profile_pic_path, "wb") as f:
+                f.write(response.content)
+            imageURL = f"/public/imgs/profile-pics/{session_id}.svg"
+        else:
+            imageURL = "/public/imgs/profile-pics/default.svg"
     else:
         existing_message = messages_collection.find_one({"session_id": session_id})
-        author = existing_message["author"] if existing_message else f"User-{session_id[:8]}"
+        if existing_message:
+            author = existing_message["author"]
+            imageURL = existing_message.get("imageURL", "/public/imgs/profile-pics/default.svg")
+        else:
+            author = f"User-{session_id[:8]}"
+            response = requests.get(f"https://api.dicebear.com/9.x/croodles-neutral/svg?seed={session_id}")
+            if response.status_code == 200:
+                profile_pic_dir = "public/imgs/profile-pics"
+                os.makedirs(profile_pic_dir, exist_ok=True)
+                profile_pic_path = f"{profile_pic_dir}/{session_id}.svg"
+                with open(profile_pic_path, "wb") as f:
+                    f.write(response.content)
+                imageURL = f"/public/imgs/profile-pics/{session_id}.svg"
+            else:
+                imageURL = "/public/imgs/profile-pics/default.svg"
 
     message_id = str(uuid.uuid4())
-    message = {"id": message_id,"session_id": session_id,"author": author,"content": content,"updated": False,"emoji":""}
+    message = {"id": message_id,"session_id": session_id,"author": author,"content": content,"updated": False,"reactions": '',"imageURL": imageURL}
     messages_collection.insert_one(message)
 
     if "Cookie: " not in request.headers:
-        res.cookies({"session":session_id})
+        res.cookies({"session": session_id})
     res.text("message sent")
     handler.request.sendall(res.to_data())
 
 def get_chats(request, handler):
     all_messages = list(messages_collection.find({}, {"_id": 0, "session": 0}))
+    # all_reactions = list(messages_collection.find({}, {"reactions": 0, "session": 0}))
     session_id = request.cookies.get("session")
     data = {"messages": all_messages}
     res = Response().json(data)
@@ -128,31 +155,84 @@ def delete_chat(request, handler):
     res = Response().text("Message deleted successfully.")
     handler.request.sendall(res.to_data())
 
-def add_emoji(request, handler):
-    chat_id = request.path.split("/")[-1]
+def add_reaction(request, handler):
+    message_id = request.path.split("/")[-1]
     data = json.loads(request.body.decode())
-    new_content = html.escape(data["emoji"])
+    emoji = html.escape(data["emoji"])
 
-    message = messages_collection.find_one({"id": chat_id})
+    message = messages_collection.find_one({"id": message_id})
+    session_id = request.cookies.get("session")
+    if not session_id:
+        res = Response().set_status(403, "Forbidden").text("Forbidden.")
+        handler.request.sendall(res.to_data())
+        return
+
+    if not isinstance(message.get("reactions"), dict):
+        message["reactions"] = {}
+
+    if emoji in message["reactions"] and session_id in message["reactions"][emoji]:
+        res = Response().set_status(403, "Forbidden").text("You already reacted with this emoji.")
+        handler.request.sendall(res.to_data())
+        return
+
+    message["reactions"].setdefault(emoji, []).append(session_id)
+    messages_collection.update_one({"id": message_id}, {"$set": {"reactions": message["reactions"]}})
+
+    res = Response().text("Reaction added successfully.")
+    handler.request.sendall(res.to_data())
+
+def remove_reaction(request, handler):
+    message_id = request.path.split("/")[-1]
+    data = json.loads(request.body.decode())
+    emoji = html.escape(data["emoji"])
+
+    message = messages_collection.find_one({"id": message_id})
     if not message:
         res = Response().set_status(404, "Not Found").text("Message not found.")
         handler.request.sendall(res.to_data())
         return
 
     session_id = request.cookies.get("session")
-    if session_id != message["session_id"]:
-        res = Response().set_status(403, "Forbidden").text("You can only update your own messages.")
+    if not session_id:
+        res = Response().set_status(403, "Forbidden").text("Forbidden.")
         handler.request.sendall(res.to_data())
         return
 
-    messages_collection.update_one({"id": chat_id}, {"$set": {"emoji": new_content}})
-    res = Response().text("Message updated successfully.")
+    reactions = message.get("reactions", {})
+    if emoji not in reactions or session_id not in reactions[emoji]:
+        res = Response().set_status(403, "Forbidden").text("You can only remove your own reactions.")
+        handler.request.sendall(res.to_data())
+        return
+
+    reactions[emoji].remove(session_id)
+    if not reactions[emoji]:
+        del reactions[emoji]
+
+    messages_collection.update_one({"id": message_id}, {"$set": {"reactions": reactions}})
+    res = Response().text("Reaction removed successfully.")
     handler.request.sendall(res.to_data())
 
 
-# def remove_emoji(request, handler):
-# def get_emoji(request, handler):
+def update_nickname(request, handler):
+    session_id = request.cookies.get("session")
+    data = json.loads(request.body.decode())
+    new_nickname = html.escape(data.get("nickname", "").strip())
 
+    if not new_nickname:
+        res = Response().set_status(400, "Bad Request").text("Nickname cannot be empty.")
+        handler.request.sendall(res.to_data())
+        return
+
+    existing_name = messages_collection.find_one({"author": new_nickname})
+    if existing_name:
+        res = Response().set_status(400, "Bad Request").text("Nickname exists.")
+        handler.request.sendall(res.to_data())
+        return
+
+    messages_collection.update_many({"session_id": session_id},{"$set": {"author": new_nickname}})
+
+    res = Response().text("Nickname updated successfully.")
+    handler.request.sendall(res.to_data())
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
@@ -167,9 +247,9 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         self.router.add_route("GET", "/api/chats", get_chats,False)
         self.router.add_route("PATCH", "/api/chats/", update_chat,False)
         self.router.add_route("DELETE", "/api/chats/", delete_chat,False)
-        self.router.add_route("PATCH", "/api/reaction/", add_emoji, False)
-        # self.router.add_route("DELETE", "/api/reaction/", remove_emoji, False)
-        # self.router.add_route("GET", "/api/chat/", get_emoji, False)
+        self.router.add_route("PATCH", "/api/reaction/", add_reaction, False)
+        self.router.add_route("DELETE", "/api/reaction/", remove_reaction, False)
+        self.router.add_route("PATCH", "/api/nickname", update_nickname, False)
         super().__init__(request, client_address, server)
 
 
